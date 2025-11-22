54c2daf2415522c1ef197b7ca2005d6e13298b14
stoyanK7 <stoyank127@gmail.com>
2025-11-22 14:35:17 +0100

    minor: script to jdk19

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 OUTPUT="test"
 while  [ -n "$OUTPUT" ]; do
-    OUTPUT=$(.ci/no-exception-test.sh openjdk17-with-checks-nonjavadoc-error | grep "Caused by: " | grep -oh '/.*.java' | sed 's./.[\\\\\\/].g' | uniq)
+    OUTPUT=$(.ci/no-exception-test.sh openjdk19-with-checks-nonjavadoc-error | grep "Caused by: " | grep -oh '/.*.java' | sed 's./.[\\\\\\/].g' | uniq)
     echo "$OUTPUT"
-    echo -e "<module name=\"BeforeExecutionExclusionFileFilter\">\n  <property name=\"fileNamePattern\" value=\"$OUTPUT\$\"/>\n</module>" >> config/projects-to-test/openjdk17-excluded.files
+    echo -e "<module name=\"BeforeExecutionExclusionFileFilter\">\n  <property name=\"fileNamePattern\" value=\"$OUTPUT\$\"/>\n</module>" >> config/projects-to-test/openjdk19-excluded.files
     rm -rf .ci-temp/contribution
     git add .
     git commit -m "added another file filter"

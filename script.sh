068457dfdb1ffd4271e1f0610dd4bf8e3cc756fc
stoyanK7 <stoyank127@gmail.com>
2025-11-22 16:25:16 +0100

    minor: jdk25

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 OUTPUT="test"
 while  [ -n "$OUTPUT" ]; do
-    OUTPUT=$(.ci/no-exception-test.sh openjdk20-with-checks-nonjavadoc-error | grep "Caused by: " | grep -oh '/.*.java' | sed 's./.[\\\\\\/].g' | uniq)
+    OUTPUT=$(.ci/no-exception-test.sh openjdk25-with-checks-nonjavadoc-error | grep "Caused by: " | grep -oh '/.*.java' | sed 's./.[\\\\\\/].g' | uniq)
     echo "$OUTPUT"
-    echo -e "<module name=\"BeforeExecutionExclusionFileFilter\">\n  <property name=\"fileNamePattern\" value=\"$OUTPUT\$\"/>\n</module>" >> config/projects-to-test/openjdk20-excluded.files
+    echo -e "<module name=\"BeforeExecutionExclusionFileFilter\">\n  <property name=\"fileNamePattern\" value=\"$OUTPUT\$\"/>\n</module>" >> config/projects-to-test/openjdk25-excluded.files
     rm -rf .ci-temp/contribution
     git add .
     git commit -m "added another file filter"
